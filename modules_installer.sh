728cb6cb022fd6bc1cc9228fb0d66655f39fba40
dylan <dylan@8cb4c0b1-acda-42e5-8754-cd0399de7e65>
2010-02-23 23:43:51 +0000

    dl command is deprecated
    git-svn-id: https://svn.metaltoad.com/svn/mtm_tools/drupal_profiles/metaltoad/trunk@12 8cb4c0b1-acda-42e5-8754-cd0399de7e65

--- modules_installer.sh
+++ modules_installer.sh
@@ -2,40 +2,40 @@
 
 shopt -s expand_aliases  # Enables alias expansion.
 
-drush dl --package-handler=cvs drupal
+drush pm-download --package-handler=cvs drupal
 cd drupal
 mkdir -p sites/all/modules/contrib
 
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib admin_menu
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib cvs_deploy
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib better_formats
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib diff
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib hierarchical_select
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib advanced_help
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib ie6update
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib role_delegation
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib adminrole
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib override_node_options
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib cck
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib views
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib filefield
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib transliteration
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib imagefield
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib imagecache
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib imageapi
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib date
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib pathauto
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib token
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib globalredirect
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib path_redirect
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib xmlsitemap
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib menu_block
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib search404
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib search_config
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib devel
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib coder
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib masquerade
-drush dl --package-handler=cvs --destination=sites/all/modules/contrib views_bulk_operations
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib admin_menu
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib cvs_deploy
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib better_formats
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib diff
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib hierarchical_select
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib advanced_help
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib ie6update
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib role_delegation
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib adminrole
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib override_node_options
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib cck
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib views
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib filefield
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib transliteration
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib imagefield
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib imagecache
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib imageapi
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib date
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib pathauto
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib token
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib globalredirect
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib path_redirect
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib xmlsitemap
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib menu_block
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib search404
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib search_config
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib devel
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib coder
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib masquerade
+drush pm-download --package-handler=cvs --destination=sites/all/modules/contrib views_bulk_operations
 
-drush dl --package-handler=cvs zen
-drush dl --package-handler=cvs rootcandy
+drush pm-download --package-handler=cvs zen
+drush pm-download --package-handler=cvs rootcandy
